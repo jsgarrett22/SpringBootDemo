dae6a806037b5747e5f11a07bfec5a7082110cf5
Joshua Garrett <garrej01@email.franklin.edu>
2026-01-18 15:17:58 -0500

    Introduced the component annotation with the concept of using beans from the IoC container to practice dependency injection

---
+++ src/main/java/com/garrett/SpringDemoProject/Dev.java
@@ -0,0 +1,14 @@
+package com.garrett.SpringDemoProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+	
+	public Dev() {}
+	
+	public void build() {
+		System.out.println("Working on awesome project.");
+	}
+
+}
--- src/main/java/com/garrett/SpringDemoProject/SpringDemoProjectApplication.java
+++ src/main/java/com/garrett/SpringDemoProject/SpringDemoProjectApplication.java
@@ -2,12 +2,24 @@ package com.garrett.SpringDemoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoProjectApplication {
 
+	/**
+	 * The main starting point of the Spring Boot application. Creates the Spring IoC container.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoProjectApplication.class, args);
+		// Creates the Spring IoC container and assigns the container
+		ConfigurableApplicationContext context = SpringApplication.run(SpringDemoProjectApplication.class, args);
+		
+		// Get a bean from the context
+		Dev dev = context.getBean(Dev.class);
+		
+		// Invoke method from obj given by bean
+		dev.build();
 	}
 
 }
